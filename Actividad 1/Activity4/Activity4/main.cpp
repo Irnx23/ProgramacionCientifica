f3ab082c2c25215322976079508852b10c5a8ea7
SamanthaLopezC <samantha.lopezco@uanl.edu.mx>
2023-03-01 20:42:23 -0600

    Update main.cpp

--- Actividad 1/Activity4/Activity4/main.cpp
+++ Actividad 1/Activity4/Activity4/main.cpp
@@ -3,19 +3,20 @@
 //  Activity4
 //
 //  Created by Itzel Robles on 19/02/23.
-//
+// Revisado by Samantha Lopez
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int A;
+    int A=0;
     for(int i=8;i<24;i++){
         cout<<"Ingresa el numero "<<i<<endl;
         cin>>A;
         if (A!=i){
             cout<<"error"<<endl;
-            //Revisar con sam como le hizo para quedar sobre el mismo numero
+            //se decrementa el valor de i para regresar al valor donde se realizo el error
+            i--;
         }
     }
     return 0;
