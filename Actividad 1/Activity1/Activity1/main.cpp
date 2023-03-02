d914323ef9378be0df13c429b518989c9c1a928f
SamanthaLopezC <samantha.lopezco@uanl.edu.mx>
2023-03-01 19:56:55 -0600

    Update main.cpp

--- Actividad 1/Activity1/Activity1/main.cpp
+++ Actividad 1/Activity1/Activity1/main.cpp
@@ -3,21 +3,29 @@
 //  Activity1
 //
 //  Created by Itzel Robles on 16/02/23.
-//
+//  Revision by Samantha Lopez
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int A,B,C;
     cout<<"Altura"<<endl;
+    //iniciar variables con valor
+    int A=0;
     cin>>A;
     cout<<"Base"<<endl;
+    int B=0;
     cin>>B;
     
+    //calculamos perimetro
+    int C=0;
     C=(2*A+2*B);
     cout<<"El perimetro del rectangulo: "<< C <<endl;
     
+    //calculamos area
+    int Area=0;
+    Area = A*B;
+    cout<<"El area del rectangulo: "<< Area <<endl;
     
     return 0;
 }
