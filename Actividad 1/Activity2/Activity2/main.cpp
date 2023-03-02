0be00b780a4f58eda2eee6a495139f07b45b3a18
SamanthaLopezC <samantha.lopezco@uanl.edu.mx>
2023-03-01 20:35:04 -0600

    Update main.cpp

--- Actividad 1/Activity2/Activity2/main.cpp
+++ Actividad 1/Activity2/Activity2/main.cpp
@@ -4,21 +4,23 @@
 //
 //  Created by Itzel Robles on 16/02/23.
 //
-
+//programa para obtener el promedio de 5 numeros enteros que se ingresen
 #include <iostream>
 using namespace std;
 
 int main() {
-    int N,S=0;
+    int N=0,S=0;
     
     for (int i=0;i<5;i++){
         cout<<"Ingresa un numero entero "<<i+1<<endl;
         cin>>N;
         S+=N;
-        
     }
+    
+    //calcular promedio de los numeros
     float promedio=(float)S/5;
-    cout<<"El proemedio es: "<<promedio<<endl;
+   
+    cout<<"El promedio es: "<<promedio<<endl;
     
     return 0;
 }
