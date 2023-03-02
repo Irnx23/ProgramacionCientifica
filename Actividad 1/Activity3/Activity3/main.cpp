1c5294ff5d78736847d6f7230c7808921e173039
SamanthaLopezC <samantha.lopezco@uanl.edu.mx>
2023-03-01 20:37:47 -0600

    Update main.cpp

--- Actividad 1/Activity3/Activity3/main.cpp
+++ Actividad 1/Activity3/Activity3/main.cpp
@@ -9,16 +9,21 @@
 #include <cmath>
 using namespace std;
 int main() {
-    float x1,x2,y1,y2, dist;
     cout<<"Ingrese coordenada x del punto A"<<endl;
+    double x1=0;
     cin>>x1;
     cout<<"Ingrese coordenada y del punto A"<<endl;
+    double y1=0;
     cin>>y1;
     cout<<"Ingrese coordenada x del punto B"<<endl;
+    double x2=0;
     cin>>x2;
     cout<<"Ingrese coordenada y del punto B"<<endl;
+    double y2=0;
     cin>>y2;
     
+    //operaciones a realizar
+    double dist=0;
     dist=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
     cout<<"Distancia entre A y B es: "<<dist<<endl;
     
