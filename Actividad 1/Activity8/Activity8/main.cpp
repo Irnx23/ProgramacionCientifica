17a527cbe0c73d9686cfe7e817b7e862f3301b83
SamanthaLopezC <samantha.lopezco@uanl.edu.mx>
2023-03-01 20:52:57 -0600

    Update main.cpp

--- Actividad 1/Activity8/Activity8/main.cpp
+++ Actividad 1/Activity8/Activity8/main.cpp
@@ -9,12 +9,17 @@
 using namespace std;
 
 int main() {
-    unsigned long N,F=1;
     cout<<"Ingrese un valor entero ";
+    unsigned long N=0;
     cin>>N;
+    
+    unsigned long F=1;
     for (int i=1;i<=N;i++){
+        //F = F*i
         F*=i;
     }
+    
+    //limite encontrado, solo se puede obtener el factorial del 1 hasta el 33
     cout<<"El factorial de: "<<N<<" es: "<<F<<endl;
     return 0;
 }
